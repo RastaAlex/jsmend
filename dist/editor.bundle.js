01f21225134b3a1cac05a78334558b98dbaf9578
RastaAlex <Rastaalex86@gmail.com>
2021-02-17 19:42:37 +0200

    try to connect

--- dist/editor.bundle.js
+++ dist/editor.bundle.js
@@ -16430,31 +16430,23 @@
     ];
 
     let startState = EditorState.create({
-      doc: "The document\nis\nshared",
       extensions: basicSetup
     });
 
     let views = [];
-    let syncAnnotation = Annotation.define();
-    function syncDispatch(from, to) {
-        return  (tr, Transaction) => {
-            views[from].update([tr]);
-            if (!tr.changes.empty && !tr.annotation(syncAnnotation))
-                views[to].dispatch({ changes: tr.changes,
-                    annotations: syncAnnotation.of(true) });
-        };
-    }
+
+    Annotation.define();
 
     views.push(
       new EditorView({
         state: startState,
-        parent: document.querySelector("#editor1"),
-        dispatch: syncDispatch(0, 1)
+        parent: document.querySelector('#editorRow'),
+        //dispatch: syncDispatch(0, 1)
       }),
       new EditorView({
         state: startState,
-        parent: document.querySelector("#editor2"),
-        dispatch: syncDispatch(1, 0)
+        parent: document.querySelector('#editorTransformed'),
+        //dispatch: syncDispatch(1, 0)
       })
     );
 
