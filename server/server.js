e128c2b57c9483d50fcdda0e233724f48aad6b33
RastaAlex <Rastaalex86@gmail.com>
2021-03-30 19:53:52 +0300

    requests

--- server/server.js
+++ server/server.js
@@ -7,16 +7,15 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 4030;
 const putout = require('putout');
+const pullout = require('pullout');
 const dword = require('dword');
 
 app.use(express.static(join(__dirname, '..')));
 app.use(dword());
 
-
-// app.get('/transform.html', (req, res) => {
-//     res.send('hello');
-//     console.log('hello');
-// });
+app.post('/transform', (req, res) => { 
+    res.send('hello');
+});
 
 http.listen(port, () => {
     console.log('server listen on port 4030');
