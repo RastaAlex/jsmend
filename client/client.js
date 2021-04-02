0035b076d76000fb9070eb61bafdbe8f860cdb30
RastaAlex <Rastaalex86@gmail.com>
2021-04-02 20:00:44 +0300

    putout/editors

--- client/client.js
+++ client/client.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 const editorRawEl = document.getElementById('js-editor-raw');
 const editorTransformedEl = document.getElementById('js-editor-transformed');
 const button = document.getElementById('transform');
@@ -12,11 +13,19 @@ async function main() {
     editorRaw.setModeForPath('hello.js');
     editorTransformed.setModeForPath('hello.js');
 
-    button.addEventListener('click', () => {
-        const valueEditorRaw = editorRaw.getValue();
-        editorTransformed.setValue(valueEditorRaw);
-        console.log(valueEditorRaw);
+    button.addEventListener('click', async () => {
+         const valueEditorRaw = editorRaw.getValue();
+        // editorTransformed.setValue(valueEditorRaw);
+         //editorTransformed.setValue(data);
+
+        const {code, places} = await request('http://localhost:4030/transform', 'POST', valueEditorRaw);
+        editorTransformed.setValue(code);
+        console.log(places);
+        
+        // request('http://localhost:4030/transform', 'GET')
+        // .then((data) => console.log(data));
     }); 
+    
 }
 
 function getEditors() {
@@ -38,4 +47,15 @@ function getEditors() {
         });
     });
 }
+
+async function request (url, method, data = null) {
+  const response = await fetch(url, {
+    method: method,
+    headers: {'Content-Type': 'application/json'},
+    body: data
+  })
+
+  return await response.json();
+}
+
 
--- server/server.js
+++ server/server.js
@@ -7,14 +7,32 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 4030;
 const putout = require('putout');
+const parseOptions = require('putout/parse-options');
+const tryCatch = require('try-catch');
 const pullout = require('pullout');
 const dword = require('dword');
+const {parseError} = require('./parse-error');
 
 app.use(express.static(join(__dirname, '..')));
 app.use(dword());
 
-app.post('/transform', (req, res) => { 
-    res.send('hello');
+app.post('/transform', async (req, res) => {
+    const data = await pullout(req);
+    const [error, result] = tryCatch(putout, data, parseOptions());
+    if (error) {
+        const places = parseError(error, {
+            debug: true
+        });
+
+        res.send({
+            places,
+            code: ''
+        });
+        
+        return;
+    }
+
+    res.send(result);
 });
 
 http.listen(port, () => {
